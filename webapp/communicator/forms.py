1a22b383e65018e87634aa7783faadba74bd10de
Linkesh Diwan <swiftarrow9@gmail.com>
2013-10-31 00:09:01 +0100

    Revert "Merge pull request #14 from harishnavnit/requirements"
    This reverts commit 5b6d97e2e030c50634cfc927bf2fe1bf5a087284, reversing
    changes made to e940d56238c2a91a0b1be9950883b91dbc178a38.

--- webapp/communicator/forms.py
+++ webapp/communicator/forms.py
@@ -1,11 +1,3 @@
-"""
-Filename: communicator/forms.py
-Authors: Bithin Alangot Seshagiri Prabhu Harish Navnit
-Copyright: Wise Earth Technology
-This file is part of the CrisisCommunicator Project.  
-It is licensed under the Peaceful Open Source License.  
-Please see the license terms in PeaceOSL.txt
-"""
 from django.forms import ModelForm
 from django import forms
 #from django.core import Validator
--- webapp/crisis/models.py
+++ webapp/crisis/models.py
@@ -1,11 +1,3 @@
-"""
-Filename: crisis/models.py
-Authors: Bithin Alangot Seshagiri Prabhu Harish Navnit
-Copyright: Wise Earth Technology
-This file is part of the CrisisCommunicator Project.  
-It is licensed under the Peaceful Open Source License.  
-Please see the license terms in PeaceOSL.txt
-"""
 from django.db import models
 #from refugeecenter.models import RefugeeCenter
 
